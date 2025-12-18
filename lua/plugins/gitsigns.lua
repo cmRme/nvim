803f774967c7bcb15e3fe609fac2ab44b55fb78e
vladimir pozov <vovasp@gmail.com>
2025-12-18 13:04:59 +0200

    Add gitsigns keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,1 +1,40 @@
-require("gitsigns").setup()
+-- require("gitsigns").setup()
+require("gitsigns").setup({
+	signs = {
+		add = { text = "+" },
+		change = { text = "~" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+	},
+
+	current_line_blame = false,
+
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+
+		local opts = { buffer = bufnr, noremap = true, silent = true }
+
+		-- toggle git signs
+		vim.keymap.set(
+			"n",
+			"<leader>gs",
+			gs.toggle_signs,
+			vim.tbl_extend("force", opts, { desc = "Git: toggle signs" })
+		)
+
+		-- preview hunk
+		vim.keymap.set(
+			"n",
+			"<leader>gp",
+			gs.preview_hunk,
+			vim.tbl_extend("force", opts, { desc = "Git: preview hunk" })
+		)
+
+		-- reset hunk
+		vim.keymap.set("n", "<leader>gr", gs.reset_hunk, vim.tbl_extend("force", opts, { desc = "Git: reset hunk" }))
+
+		-- blame current line
+		vim.keymap.set("n", "<leader>gb", gs.blame_line, vim.tbl_extend("force", opts, { desc = "Git: blame line" }))
+	end,
+})
